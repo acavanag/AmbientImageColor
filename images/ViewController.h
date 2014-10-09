fd979e7178dd7a8e405cf33d36f8647e2aab6506
acavanaghww <acavanagh@weddingwire.com>
2014-10-09 11:45:12 -0400

    Initial Commit

---
+++ images/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  images
+//
+//  Created by Andrew Cavanagh on 10/9/14.
+//  Copyright (c) 2014 WeddingWire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ images/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  images
+//
+//  Created by Andrew Cavanagh on 10/9/14.
+//  Copyright (c) 2014 WeddingWire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
